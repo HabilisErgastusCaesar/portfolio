6e08d6d63d92a3598bd4bcfe5fca0c943a6f74f1
HabilisErgastusCaesar <bartscheepmaker@hotmail.com>
2025-09-03 07:23:02 +0200

    Update [stargatePage].tsx

--- pages/stargate/[stargatePage].tsx
+++ pages/stargate/[stargatePage].tsx
@@ -1,27 +1,17 @@
-import { useEffect } from "react"
 import { useRouter } from "next/router"
 
-import { useStargateContext } from "../../components/stargate/useContext/stargateComponents";
 import { MainLayout } from "../../components/stargate/mainLayout";
 import { UpperBar } from "../../components/stargate/upperBar";
 
 const StargatePage = () => {
     const router = useRouter();
-    const { Context } = useStargateContext();
-
-    useEffect(() => {
-        if (router.query.stargatePage) {
-            console.log("kenker zooi")
-        };
-
-        return () => {}
-    }, [router])
     
     return(<div>
-        <UpperBar />
-        <MainLayout />
+        {router.query.stargatePage && <>
+            <UpperBar router={router.query.stargatePage}/>
+            <MainLayout router={router.query.stargatePage}/>
+        </>}
     </div>)
 }
 
 export default StargatePage
-
